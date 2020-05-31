d3b706ae7dbafe213e70cde7f325e0a2f6aaeb25
Karolina Suliga <karolina.suliga98@gmail.com>
2020-05-31 18:40:16 +0200

    added saving presets to file

--- app/src/main/java/com/example/smartgreenhouse/MainMenu.java
+++ app/src/main/java/com/example/smartgreenhouse/MainMenu.java
@@ -9,25 +9,34 @@ import android.view.View;
 import android.widget.Button;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.LinkedList;
 
 public class MainMenu extends AppCompatActivity {
 
     public static final LinkedList<Preset> listOfPresets = new LinkedList<Preset>();
-    File presetFile = new File(getApplicationContext().getFilesDir(), "presets.txt");
+    File presetFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        presetFile = new File(MainMenu.this.getFilesDir(), "presets.txt");
 
         try {
             getPresetsList();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            try {
+                presetFile.createNewFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
 
         Button button_add = (Button)findViewById(R.id.button2);
@@ -67,16 +76,45 @@ public class MainMenu extends AppCompatActivity {
         });
     }
 
-    private void getPresetsList() throws FileNotFoundException {
-        BufferedReader reader = new BufferedReader(new FileReader(presetFile.getPath()));
+    protected void onStop() {
+        super.onStop();
+
+        BufferedWriter br = null;
+        try {
+            br = new BufferedWriter(new FileWriter(presetFile));
+            PrintWriter pw = new PrintWriter(presetFile.getPath());
+            pw.close();
+            for(int i = 0; i < listOfPresets.size()-1; i++)
+            {
+                Preset pr = listOfPresets.get(i);
+                String name = pr.getName();
+                String temp =  String.valueOf(pr.getTemperature());
+                String humidity = String.valueOf(pr.getHumidity());
+                br.write(name,0, name.length());
+                br.newLine();
+                br.write(temp,0, temp.length());
+                br.newLine();
+                br.write(humidity,0, humidity.length());
+                br.newLine();
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getPresetsList() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(presetFile));
         String line = null;
         while((line = reader.readLine()) != null)
         {
             Preset pr = new Preset();
             pr.setName(line);
-            pr.setTemperature(Integer.getInteger(reader.readLine()));
-            pr.setHumidity(Integer.getInteger(reader.readLine()));
+            pr.setTemperature(Integer.parseInt(reader.readLine()));
+            pr.setHumidity(Integer.parseInt(reader.readLine()));
+            listOfPresets.addLast(pr);
         }
+        reader.close();
     }
 
     private void goToAddPreset()
